d304f40985daba9f2ecb7980503eb7ac159cecc5
tiago19deoliveira <tiago18deoliveira@gmail.com>
2025-10-28 22:24:36 -0400

    feat: iniciando configurações e conexão com bacno

---
+++ src/database/mongoose.database.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const connectToDatabase = async () => {
+    await mongoose.connect(
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fsctaskmanagercluster.c2oprob.mongodb.net/?appName=FscTaskManagerCluster`,
+        console.log("Connected to MongoDb")
+    );
+};
+
+module.exports = connectToDatabase;
