4bb311530ba66053a2455908277c377c84fb0cc1
tiago19deoliveira <tiago18deoliveira@gmail.com>
2025-11-02 14:22:39 -0400

    feat: get task by id

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ app.get("/tasks", async (req, res) => {
     }
 });
 
+app.get("/tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const task = await TaskModel.findById(taskId);
+
+        if (!task) {
+            return res.status(404).send("Essa tarefa não foi encontrada!");
+        }
+
+        res.status(200).send(task);
+    } catch (e) {
+        res.status(500).send(error.message);
+    }
+});
+
 app.post("/tasks", async (req, res) => {
     try {
         const newTask = new TaskModel(req.body);
